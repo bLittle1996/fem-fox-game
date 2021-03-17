834a7646753600dd8bb75e5cee0f78f34fd66103
Ben Littleton <b.little1996@gmail.com>
2021-03-17 18:26:00 -0400

    Created a guards file to add generic guards to. Currently isDefined and isNumber are prsent

---
+++ server/src/utils/guards.ts
@@ -0,0 +1,7 @@
+export const isDefined = <T>(thing: T): thing is Exclude<T, undefined> => {
+    return typeof thing !== 'undefined';
+}
+
+export const isNumber = (thing: unknown): thing is number => {
+    return typeof thing === 'number';
+}
